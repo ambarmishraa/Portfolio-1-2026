337ce1cc1a58088a92c16d606b26adc1feb93dbd
ambarmishraa <ambarmishra740@gmail.com>
2026-02-03 04:07:38 +0100

    project initalise

---
+++ src/data/experienceData.js
@@ -0,0 +1,26 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Mobiloitte',
+        jobtitle: 'Software Developer Intern',
+        startYear: 'Feb',
+        endYear: 'April 2024'
+    },
+    {
+        id: 2,
+        company: 'Topperworld',
+        jobtitle: 'Full Stack Developer Intern',
+        startYear: 'Aug 2023',
+        endYear: 'Jan 2024'
+    },
+    {
+        id: 3,
+        company: 'TechnoHacks EduTech',
+        jobtitle: 'Java Developer Intern',
+        startYear: 'Sept',
+        endYear: 'Oct 2023'
+    },
+    
+    
+    
+]
